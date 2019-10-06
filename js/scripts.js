4d6ebd3bd3cf69b7b775c0492765a220e82e1502
wilbrone <wilbroneokoth@gmail.com>
2019-10-07 01:35:37 +0300

    addition function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
-
+function Order(name,phone,size,crust,toppings ){
+    this.Name = name;
+    this.Phone = phone;
+    this.Size = size;
+    this.Crust = crust;
+    this.Toppings = toppings;
+}
 
 
 $("#orders").click(function(){
@@ -8,42 +14,70 @@ $("#orders").click(function(){
 $("form#new-order").submit(function(event) {
     event.preventDefault();
 
+    // the data from the form
     var name = $("input#name").val();
-    console.log(name);
     var phoneNo = $("input#phone").val();
-    console.log(phoneNo);
 
-    var toppings = [];
-    $.each($("input[name='topping']:checked"), function(){
-        toppings.push($(this).val());
-    });
-    console.log(toppings);
-
-    // var topping = $('input[name=topping]').val();
     var crust = $("select#inputGroupSelect02").val();
-    console.log(crust);
     var size = $("select#inputGroupSelect01").val();
-    console.log(size);
+    var top = [];
+    $.each($("input[name='topping']:checked"), function(){
+        top.push($(this).val());
+    });
 
     var street = $("input#streetName").val();
-    console.log(street);
     var estate = $("input#estate").val();
-    console.log(estate);
     var apartment = $("input#apartment").val();
-    console.log(apartment);
     var floor = $("input#floor").val();
-    console.log(floor);
-    // var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+
+    // Structure of th constructor ***Order(name,phone,size,crust,toppings )***
+    // creating the object for the order made by the user
+    var newOrder = new Order(name,phoneNo,size,crust,top);
+    console.log(newOrder);
+
+    billCalculation(newOrder);
+
+
+
 
     // this is for emptying the fields after clicking submit
     resetFields();
 
 });
 
+
+// CALCULATION FUNCTION
+function billCalculation(newOrder){
+    var sizePrice = parseInt(newOrder.Size.split(", ")[1]);
+    console.log(sizePrice);
+
+    var crustPrice = parseInt(newOrder.Crust.split(", ")[1]);
+    console.log(crustPrice);
+
+    
+    var topp = newOrder.Toppings;
+    console.log(topp);
+
+    var toppingPrice = 0;
+
+    // for getting toppings
+    for(var i = 0; i < topp.length; i++ ){
+        var unitPrice = parseInt(topp[i].split(", ")[1]);
+  
+        toppingPrice += unitPrice;
+    }
+    console.log(toppingPrice);
+
+    var price = sizePrice + crustPrice + toppingPrice;
+    console.log(price);
+}
+
+// for emptying the fields
 function resetFields(){
     $("input#name").val("");
     $("input#phone").val("");
-    $("select#inputGroupSelect03").val("");
+    $("input[name='topping']").prop('checked',false);
     $("select#inputGroupSelect02").val("");
     $("select#inputGroupSelect01").val("");
 };
